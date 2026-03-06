3bf5a06d1eb37adbe0d4a0c87c4b46aadad06006
Wisarut Tiyamanee <jtiyamanee@hotmail.com>
2026-03-06 18:02:11 +0700

    create actions and empty pages

---
+++ src/actions/create-post.ts
@@ -0,0 +1,5 @@
+"use server";
+
+export async function createPost() {
+  // TODO: revalidates topic show page after creating a post
+}
---
+++ src/actions/create-topic.ts
@@ -0,0 +1,5 @@
+"use server";
+
+export async function createTopic() {
+  // TODO: revalidates home page after creating a topic
+}
